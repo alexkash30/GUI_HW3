f1b33bae28694917e2a16ebfd01ec3e7789ce015
Alex Kashian 01895339 <66210717+alexkash30@users.noreply.github.com>
2023-11-14 21:45:00 -0500

    Add files via upload

--- JS/add-content.js
+++ JS/add-content.js
@@ -10,7 +10,6 @@ function myFunction() {
 	var table = document.getElementById("myTable");
 	table.innerHTML = "";
 
-	//grab all the inputs from the users
 	let minC = document.getElementById("minCol").value;
 	let maxC = document.getElementById("maxCol").value;
 	let minR = document.getElementById("minRow").value;
@@ -21,10 +20,8 @@ function myFunction() {
 	console.log(minR);
 	console.log(maxR);
 
-	//grab the error message to update in case of error 
 	let errorMessage = document.getElementById("errorMessage");
 
-	//see if any of the inputs are blank
 	if (minC == "" || maxC == "" || minR == "" || maxR == "")
 	{
 		errorMessage.textContent = "Missing Information In One Of The Fields!";
@@ -33,31 +30,22 @@ function myFunction() {
 	}else {
 		errorMessage.textContent = "";
 	}
-
-	//change the strings to numbers
-	minC = Number(minC);
-	maxC = Number(maxC);
-	minR = Number(minR);
-	maxC = Number(maxR);
-
-	//check to see if the inputs are in range
-	if(minC< -50 || maxC< -50 || minR< -50 || maxR< -50){
+	
+	if(Number(minC)< -50 || Number(maxC)< -50 || Number(minR)< -50 || Number(maxR)< -50){
 		errorMessage.textContent = "Input Out Of Range!";
 		errorMessage.style.color = "white";
 		return;
 	}else {
 		errorMessage.textContent = "";
 	}
-	if(minC> 50 || maxC> 50 || minR> 50 || maxR> 50){
+	if(Number(minC)> 50 || Number(maxC)> 50 || Number(minR)> 50 || Number(maxR)> 50){
 		errorMessage.textContent = "Input Out Of Range!";
 		errorMessage.style.color = "white";
 		return;
 	}else {
 		errorMessage.textContent = "";
 	}
-
-	//chack to see if the min vals are less then the max vals
-	if(minC >= maxC || minR >= maxR){
+	if(Number(minC) >= Number(maxC) || Number(minR) >= Number(maxR)){
 		errorMessage.textContent = "Invalid Input! Min Column Should be Less Then Max Column And Min Row Should be Less Then Max Row.";
 		errorMessage.style.color = "white";
 		return;
@@ -65,26 +53,22 @@ function myFunction() {
 		errorMessage.textContent = "";
 	}
 	
-	//insert top left blank square
 	var row = table.insertRow(0);
 	var cellI = row.insertCell(0);
 	cellI.innerHTML = "  ";
-
-	//insert the top row
-  	for(let a = minC; a <= maxC; a++){
+  	for(let a = Number(minC); a <= Number(maxC); a++){
 	  var cellI = row.insertCell(-1);
 	   cellI.innerHTML = a;
 	   cellI.classList.add("header-cell");
 
   	}
   
-	//insert all the values in the first coloumn then populate the rest of the table
-  	for(let i = minR; i <= maxR; i++){
+  	for(let i = Number(minR); i <= Number(maxR); i++){
 		var row = table.insertRow(-1);
 	  	var cell1 = row.insertCell(0);
 	  	cell1.innerHTML = i
   
-	  	for(let j = minC; j <=maxC; j++){
+	  	for(let j = Number(minC); j <=Number(maxC); j++){
 		  var cell1 = row.insertCell(-1);
 		  cell1.innerHTML = j*i;
   
